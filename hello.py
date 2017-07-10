b6295e6aa5a7c68aa4356d991ed3d3b8d209f716
hemu <853141976@qq.com>
2017-07-10 10:38:45 +0800

    user

--- hello.py
+++ hello.py
@@ -8,6 +8,10 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello world!</h1>\n<h2>你好啊，世界。</h2>'
 
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello %s!</h1>'%name
+
 if __name__ == '__main__':
     app.run()
 
