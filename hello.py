ee63618c4d582cc140b11b90b26db98c3e31c013
hemu <853141976@qq.com>
2017-07-10 11:28:35 +0800

    add bootstrap

--- hello.py
+++ hello.py
@@ -1,18 +1,20 @@
 #!usr/bin/env python3
 #coding = utf-8
-from flask import  Flask
+from flask import  Flask, render_template
 from flask_script import Manager
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello world!</h1>\n<h2>你好啊，世界。</h2>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello %s!</h1>'%name
+    return render_template('user.html',name = name)
 
 if __name__ == '__main__':
     manager.run()
